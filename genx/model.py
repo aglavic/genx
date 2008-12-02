0b74559b1f393190648531ab953a067e7139489a
Matts Bjoerk <mattsb>
2008-12-02 13:31:31 +0000

    Fixed misspelling in model.py

--- genx/model.py
+++ genx/model.py
@@ -417,7 +417,7 @@ class Model:
         model_copy.script_module = None
         model_copy.filename = self.filename
         model_copy.compiled = self.compiled
-        mode_copy.fom = self.fom
+        model_copy.fom = self.fom
         model_copy.saved = self.saved
         
         return model_copy
