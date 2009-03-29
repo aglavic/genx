7ea2aac84dcd4cc3a251cf883c77f706e3440c66
Matts Bjoerk <mattsb>
2009-03-29 18:59:36 +0000

    Adding the icons to version control.

---
+++ icons/reflectivity_plugin/exportimages.py
@@ -0,0 +1,15 @@
+''' 
+A utillity that exports all png files in the scripts directory 
+to a python file using img2py
+'''
+
+import wx.tools.img2py as impy
+import os
+
+files = [file for file in os.listdir('.') if file[-4:] == '.png']
+
+appending = False
+
+for file in files:
+    impy.img2py(file, 'images.py', append = appending, imgName = file[:-4])
+    appending = True
