1f7e4a3012c7aeaba74b8046c048737e40b7b4ba
Matts Bjoerk <mattsb>
2010-08-10 18:01:23 +0000

    Some minor bug fixes and included the possibility to fit the real and imagainary part of the scattergin lentgh in reflectivity models

--- genx/plugins/add_ons/Reflectivity.py
+++ genx/plugins/add_ons/Reflectivity.py
@@ -395,8 +395,9 @@ class SampleHandler:
 
 class MyHtmlListBox(wx.HtmlListBox):
 
-    def __init__(self, parent, id, style = wx.BORDER_SUNKEN):
-        wx.HtmlListBox.__init__(self, parent, id, style = wx.BORDER_SUNKEN)
+    def __init__(self, parent, id, size = (-1, -1), style = wx.BORDER_SUNKEN):
+        wx.HtmlListBox.__init__(self, parent, id, size = size, 
+                                style = wx.BORDER_SUNKEN)
         self.SetItemList(['Starting up...'])
 
     def SetItemList(self, list):
