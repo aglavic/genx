283e53d9f4ed3616928711f89a8ed0a651f42339
Matts Bjoerk <mattsb>
2015-03-22 19:26:25 +0000

    Added automatic loading of error bars (changed default to load 3 -column data file) as requested in ticket [#110]. No errors are handled with the use of nan's

--- genx/plugins/data_loaders/default.py
+++ genx/plugins/data_loaders/default.py
@@ -8,8 +8,8 @@ If y_error is not used the it can safely be set to the same column as y. <p>
 Which columns that are imported are determined from the dialog box in 
 import settings. Note that these settings apply to the marked data set(s).
 Other possible tunings are the definitions of delimiter, None means any 
-white space characthre (default). Skip rows is how many rows are skipped before 
-the file is started to be read. Comment is the first chrachter of a commented 
+white space character (default). Skip rows is how many rows are skipped before
+the file is started to be read. Comment is the first character of a commented
 line.
 '''
 
@@ -25,7 +25,7 @@ class Plugin(Template):
         Template.__init__(self, parent)
         self.x_col = 0
         self.y_col = 1
-        self.e_col = 1
+        self.e_col = 2
         self.comment = '#'
         self.skip_rows = 0
         self.delimiter = None
@@ -47,21 +47,27 @@ class Plugin(Template):
             if len(load_array.shape) < 2:
                 load_array = np.array([load_array])
             # Check so we have enough columns
-            if load_array.shape[1]-1 < max(self.x_col, self.y_col, self.e_col):
-                ShowWarningDialog(self.parent, 'The data file does not contain'\
-                        + 'enough number of columns. It has ' + str(load_array[1])\
-                        + ' columns. Rember that the column index start at zero!')
-                # Okay now we have showed a dialog lets bail out ...
+            if load_array.shape[1]-1 < max(self.x_col, self.y_col):
+                ShowWarningDialog(self.parent, 'The data file does not contain enough number of columns. It has '
+                                  + str(load_array[1]) + ' columns. Rember that the column index start at zero!')
+                self.SetStatusText('Could not load data - not enough columns')
                 return
-            # The data is set by the default Template.__init__ function, neat hu
+
+            # The data is set by the default Template.__init__ function
             # Know the loaded data goes into *_raw so that they are not
             # changed by the transforms
             self.data = self.parent.data_cont.get_data()
             self.data[data_item_number].x_raw = load_array[:, self.x_col]
             self.data[data_item_number].y_raw = load_array[:, self.y_col]
-            self.data[data_item_number].error_raw = load_array[:, self.e_col]
-            # Run the commands on the data - this also sets the x,y, error memebers
-            # of that data item.
+
+            # Check if we have errors in the data - if not handle it with nan's
+            if load_array.shape[1]-1 < self.e_col:
+                self.data[data_item_number].error_raw = load_array[:, self.y_col]*np.nan
+                self.SetStatusText('Could not load error column - setting it to nan')
+            else:
+                self.data[data_item_number].error_raw = load_array[:, self.e_col]
+
+            # Run the commands on the data - this also sets the x,y, error members of the data item.
             self.data[data_item_number].run_command()
             
             self.UpdateDataList()
