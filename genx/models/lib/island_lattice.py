9b64c5db49fbe0818b808bae2f3c0919ebce8413
Matts Bjoerk <mattsb>
2017-02-21 19:50:36 +0000

    Added a new material library based on parameters and a model based on parameters.

---
+++ genx/models/lib/island_lattice.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+.. module:: island_lattice.py
+   :synopsis: Models to calcualte the reflectivity from a 2D island array
+
+.. moduleauthor:: Matts Björck <matts.bjorck@gmail.com>
+
+This module provide functions to calculate the reflectivity from gratings using the distorted wave Born approximation
+(DWBA) and the kinematical theory.
+"""
+
+from parameters import Int, Float, ComplexArray, Func, Var, List, HasParameters
+from materials import Material
+import numpy as np
+
+
+import grating
+
+q = Var('q', np.array([0, 0, 0]), help="The scattering vector.")
+e = Var('wl', 1.54,  help="The wavelength of the radiation.")
+z = Var('z', 0.0, help="The out-of-plane position in the sample.")
+rep = Var('rep', 0, help="The repetition index in a Stack, starting with zero.")
+
+validation_kwargs = {'q': np.array([0, 0, 0]), 'z': 0, 'rep': 0}
+
+
+class Layer(HasParameters):
+    validation_kwargs = validation_kwargs
+    protected = True
+
+    def __init__(self, **kwargs):
+        self.d = Float(0, unit="AA", help="Layer thickness")
+        self.sld = ComplexArray(0, unit="1/AA^2", help="The in-plane fourier transform of the scattering length density")
+        HasParameters.__init__(self, **kwargs)
+
+
+class Stack(HasParameters):
+    validation_kwargs = validation_kwargs
+    protected = True
+
+    def __init__(self, **kwargs):
+        try:
+            value = kwargs.pop('layers')
+        except KeyError:
+            value = []
+        self.layers = List(Layer, value=value, help="The layers in the stack")
+        self.reps = Int(1, help="Number of repetitions of the stack")
+        HasParameters.__init__(self, **kwargs)
+
+    def build_layer_list(self, parameters, kwargs):
+        """Builds an array for layer parameters by looping trough the stacks and layers
+
+        Parameters:
+            parameters(list of string): name of a valid layer parameters
+            kwargs(dict): Keyword argument to be passed to the parameter call
+        """
+        out = dict()
+        [out.__setitem__(par, []) for par in parameters]
+        kwargs['rep'] = -1
+        for n in range(self.reps(**kwargs)):
+            kwargs['rep'] += 1
+            for layer in self.layers:
+                # Evaluate the parameter value
+                #print kwargs
+                [out[par].append(layer.__getattribute__(par)(**kwargs)) for par in parameters]
+                kwargs['z'] += layer.d(**kwargs)
+        kwargs.pop('rep')
+        return out
+
+
+class Sample(HasParameters):
+    validation_kwargs = validation_kwargs
+
+    def __init__(self, **kwargs):
+        try:
+            value = kwargs.pop('stacks')
+        except KeyError:
+            value = []
+        self.stacks = List(Stack, value=value, help="The stacks in the sample")
+        self.substrate = Layer(d=0.0, sld=0*FLayer())
+        self.ambient = Layer(d=0.0, sld=0*FLayer())
+        self.a = Float(2 * np.pi, unit="AA", help="Unit cell size along the x-direction")
+        self.b = Float(2 * np.pi, unit="AA", help="Unit cell size along the y-direction")
+
+        HasParameters.__init__(self, **kwargs)
+
+
+    def build_layer_list(self, parameters, kwargs):
+        """Builds an array for layer parameters by looping through the stacks and layers
+
+        Parameters:
+            parameters(list of string): name of a valid layer parameter
+            kwargs(dict): Keyword argument to be passed to the parameter call
+        """
+        kwargs['z'] = 0.0
+        out = dict()
+        [out.__setitem__(par, [self.substrate.__getattribute__(par)(**kwargs)]) for par in parameters]
+        for stack in self.stacks:
+            stack_dict = stack.build_layer_list(parameters, kwargs)
+            [out[par].extend(stack_dict[par]) for par in parameters]
+        kwargs['z'] = 0.0
+        [out[par].append(self.ambient.__getattribute__(par)(**kwargs)) for par in parameters]
+        return out
+
+    def simulate(self, instrument, h, k, qz):
+        # Add a q = 0 point for the mean scattering potential
+        q = np.c_[h*2*np.pi/self.a(), k*2*np.pi/self.b(), -qz].T
+        kwargs = {'z': 0.0, 'q': q}
+        wavelength = instrument.wl(**kwargs)
+        kwargs['wl'] = wavelength
+
+        area_uc = (self.a*self.b)(**kwargs)
+        dic = self.build_layer_list(['d', 'sld'], kwargs)
+        vf, d = 4*np.pi*np.array(dic['sld']), np.array(dic['d'])
+        kwargs['q'] = np.array([0, 0, 0])
+        vmean = 4*np.pi*np.array(self.build_layer_list(['sld'], kwargs)['sld'])/area_uc
+        print vmean.shape, vf.shape
+        k_in, k_out = instrument.reflectometer_3axis_kinout(q)
+        R = grating.coherent_refl(k_in, k_out, wavelength, vf.T, vmean, d, area_uc, kin=False)
+        return R
+
+
+class Instrument(HasParameters):
+    validation_kwargs = validation_kwargs
+
+    def __init__(self, **kwargs):
+        self.wl = Float(1.54, unit="AA", help="Wavelength of the used radiation")
+        HasParameters.__init__(self, **kwargs)
+
+    def reflectometer_3axis_kinout(self, q):
+        """Tranforms q values to k_in and k_out vectors for a reflectometer setup with a sample rotation circle.
+
+        Parameters:
+            q(array): an array for qx, qy and qz, size (3xM)
+
+        Returns:
+            k_in (array): The incomming wavevector, size (3xM)
+            k_out (array): The outgoing wavevector, size (3xM)
+        """
+        qx = q[0]
+        qy = q[1]
+        qz = q[2]
+        k = 2*np.pi/self.wl()
+        qp = np.sqrt(qx*qx + qy*qy)
+        q_abs = np.sqrt(qx*qx + qy*qy + qz*qz)
+        phi = np.arctan2(qy, qx)
+        cos_phi = np.cos(phi)
+        sin_phi = np.sin(phi)
+
+        apb = np.arcsin(q_abs/2.0/k)
+        amb = np.arctan2(qp, -qz) % np.pi
+        alpha = apb + amb
+        beta = apb - amb
+        cos_alpha = np.cos(alpha)
+        cos_beta = np.cos(beta)
+        k_in = k * np.c_[cos_alpha*cos_phi, cos_alpha*sin_phi, np.sin(alpha)].T
+        k_out = k * np.c_[cos_beta * cos_phi, cos_beta * sin_phi, -np.sin(beta)].T
+        return k_in, k_out
+
+
+class FLayer(Func):
+    """The in-plane fourier transform of an in-plane infinite layer"""
+
+    def function(self, **kwargs):
+        q = kwargs['q']
+        return np.where(np.bitwise_and(q[0] == 0, q[1] == 0), 1.0, 0.0)
+
+
+class FCircle(Func):
+    """The in-plane fourier transform of a circle"""
+
+    def __init__(self, **kwargs):
+        self.r = Float(1.0, help="The radius of the circle")
+        Func.__init__(self, **kwargs)
+
+    def function(self, **kwargs):
+        q = kwargs['q']
+        r = self.r(**kwargs)
+        q_r = np.sqrt(q[0] ** 2 + q[1] ** 2)
+        return 2 * np.pi * r ** 2 * grating.jinc(q_r * r)
+
+if __name__ == '__main__':
+    radius = Float(1.0)
+    slope = Float(1)
+    mat_Fe = Material('Fe', density=7.87)
+    Fe = Layer(d=rep*0.1 + 10.0, sld=mat_Fe.sld_x*FCircle(r=radius + z*slope))
+    Fe.d = 20.0
+    q = np.array([0, 0, 0])
+    print Fe.sld(q=q)
+    print Fe.d()
+    ML = Stack(reps=10, layers=[Fe, ])
+
+    s = Sample(stacks=[ML, ])
+    print isinstance(s, HasParameters), s.__class__.__name__
+    inst = Instrument()
+
+    print 'D with a gradient: ', s.build_layer_list(['d'], {})['d']
+
+    #print s.stacks[0].layers[0]
+    qz = np.arange(0, 0.1, 0.001)
+    s.simulate(inst, qz*0 + 0.0, qz*0 + 0.01, qz)
---
+++ genx/models/lib/materials.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+.. module:: parameters
+   :synopsis: Classes to work with differnt materials in scattering experiments
+
+.. moduleauthor:: Matts Björck <matts.bjorck@gmail.com>
+
+"""
+
+from os import path
+import re
+
+import numpy as np
+from scipy import interpolate
+
+from parameters import HasParameters, ComplexArray, Float, Calc
+
+import scatteringlengths as _sl
+
+# The scattering length of an electron (Thomson scattering length) in AA
+_r_e = 2.82e-5
+# Transformations between AA and eV
+_AA_to_eV = 12398.42
+
+
+_module_dir, _filename = path.split(__file__)
+# Look only after the file name and not the ending since
+# the file ending can be pyc if compiled...
+_filename = _filename.split('.')[0]
+_module_dir = '.' if _module_dir == '' else _module_dir
+
+
+_atomic_weights = _sl.load_atomic_weights_dabax(path.join(_module_dir, "../databases/AtomicWeights.dat"))
+
+
+def create_fpdisp_lookup(path):
+    """ Creates a lookup function to lookup element names and returns a function
+    that yields a function of dispersive scattering factors f(E) at Q = 0. NOTE energy is in eV
+    """
+    def create_dispersion_func(name):
+        e, f1, f2 = np.loadtxt(path + '%s.nff' % name.lower(), skiprows=1, unpack=True)
+        f1interp = interpolate.interp1d(e, f1, kind='linear')
+        f2interp = interpolate.interp1d(e, f2, kind='linear')
+        def_wl = _AA_to_eV/np.mean(e)
+
+        def f(wl=def_wl, **kwargs):
+            return f1interp(_AA_to_eV/wl) - 1.0J*f2interp(_AA_to_eV/wl)
+
+        return Calc(f)
+    return create_dispersion_func
+
+f0 = create_fpdisp_lookup(path.join(_module_dir, "../databases/f1f2_nist/"))
+
+
+class Material(HasParameters):
+    """ Class to keep transform a chemical formula and mass density to scattering length
+
+    The formula is given in ASCII format, for example 'Ag2(NO2)3' or 'HNO3', nested parentheses are allowed.
+
+    """
+    validation_kwargs = {'wl': 1.54}
+
+    # The scattering length should be in AA-2
+    # Need to transform g/cm3 to formula units per AA3
+    # 1u = 1.66054×10−27 kg 1 AA=10−10m
+    # density[kg/m3] = 1.66054*10^3*density[u/A3]
+    # density [g/cm3] = density [kg/m3] * 1e-3/1e-2**3 = density [kg/m3] * 1e3
+    # So formula units per AA^ = density [kg/m3]/1.66054*10^3/weight per formula unit
+    # or formula units per AA^ = density [g/cm3]/1.66054/weight per formula unit
+
+    def __init__(self, formula, **kwargs):
+        self.sld_x = ComplexArray(0, unit="1/AA-2", help="Scattering length density")
+        self.density = Float(0.0, unit="g/cm3", help="Weight density")
+        self._formula_density = Float(0.0, unit="u", help="weight of one formula unit")
+        HasParameters.__init__(self, **kwargs)
+        self.formula_dict = {}
+        self.parse_formula(formula)
+
+        # Create the composition variables needed to describe formula
+        w_list = []
+        f0_list = []
+        for name in self.formula_dict:
+            self.__setattr__(name, Float(self.formula_dict[name], help="Number of %s atoms" % name))
+            element_count = self.__getattribute__(name)
+            w_list.append(element_count*self.get_atomic_weight(name))
+            f0_list.append(element_count*self.get_f0(name))
+        self._formula_density = self.density/1.66054/sum(w_list)
+        self.sld_x = self._formula_density*sum(f0_list)*_r_e
+
+    def get_atomic_weight(self, element):
+        """Returns the atomic weight in unified atomic mass units (u)
+
+        Parameters:
+            element(string): name of the element
+
+        """
+        return _atomic_weights[element.lower()]
+
+    def get_f0(self, element):
+        """Returns the complex atomic forward scattering length normalised to the scattering length of one electron for
+         element.
+
+        Parameters:
+            element(string): name of the element
+
+        Returns:
+            f0(function): function for f0 that takes the wavelength as a keyword argument
+        """
+        return f0(element)
+
+    def parse_formula(self, formula, multiplier=1):
+        """Parses the string formula and returns a list of isotopes
+
+        Parameters:
+            formula(string): A string containing the formula.
+            multiplier(int): A number representing that will be used to multiply the element count
+        """
+        regexp = re.compile(r"([A-Z][a-z]?)(\d*)|\((.*)\)(\d*)")
+        element, element_count, group, group_count = (1, 2, 3, 4)
+        pos = 0
+        while pos < len(formula):
+            match = regexp.match(formula, pos)
+            if not match:
+                raise ValueError("Error in formula: %s at position %d" % (formula, pos))
+            pos = match.end()
+            if match.group(element):
+                print "Match %s is an element" % match.group(0)
+                count = self.formula_count(match.group(element_count), multiplier)
+                try:
+                    self.formula_dict[match.group(element)] += count
+                except KeyError:
+                    self.formula_dict[match.group(element)] = count
+                print self.formula_dict
+            elif match.group(group):
+                print "Group found: %s, count: %s" % (match.group(group), match.group(group_count))
+                count = self.formula_count(match.group(group_count), multiplier)
+                self.parse_formula(match.group(group), multiplier=count)
+
+    def formula_count(self, count_str, multiplier):
+        """Handles the parsing result for the count of an formula part. Returns the count*multiplier"
+
+        Parameters:
+            count_str(string): The count of the element/group as a string
+            multiplier(float): The  multiplier from previous groups.
+
+        """
+        if not count_str:
+            # Set count to 1 if count is not exist, ie a single group is parsed
+            count = 1
+        else:
+            count = float(count_str)
+        return count * multiplier
+
+
+
+
+if __name__ == "__main__":
+    test = Material("Fe", density=7.87)
+    print _atomic_weights['fe']
+    # print test.Ag.help
+    print test._formula_density(), 2/2.88**3
+    print test.sld_x(wl=1.54)
+    test.Fe = 2
+    print test.sld_x(wl=1.54)
+    test.Fe = 1
+    print test.sld_x(wl=1.54)
+
