6572fb2e772928cb25430fa967bdc8e564fd5718
Artur Glavic <artur.glavic@psi.ch>
2021-02-15 16:36:41 +0100

    Script to update version in inno setup script.

---
+++ genx/windows_build/update_version.py
@@ -0,0 +1,11 @@
+# Update the genx version in .iss file
+import sys
+import os
+
+folder=os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(folder))
+from genx import version
+
+txt=open(os.path.join(folder, 'genx_template.iss'), 'r').read()
+txt=txt.replace('{version}', version.version)
+open(os.path.join(folder, 'genx.iss'), 'w').write(txt)
