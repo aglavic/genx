a6f30ddafcbf3db2cb03e34cbf5ac75661190b8b
Artur Glavic <artur.glavic@psi.ch>
2021-06-03 17:07:13 +0200

    Update performance test script to compare with CUDA

--- genx/tests/full_model_performance.py
+++ genx/tests/full_model_performance.py
@@ -3,14 +3,14 @@
 import sys
 import genx.models
 sys.modules['models']=sys.modules['genx.models']
-from genx.models.lib.paratt_numba import ReflQ as nb_ReflQ
+from genx.models.lib import paratt_numba, neutron_numba, paratt_cuda, neutron_cuda
 
 import genx.models.spec_nx as model
 from genx.models.utils import UserVars, fp, fw, bc, bw
 from numpy import *
 
 # BEGIN Instrument DO NOT CHANGE
-inst = model.Instrument(footype = 'gauss beam',probe = 'neutron',beamw = 0.2,resintrange = 3,tthoff = 0.0,
+inst = model.Instrument(footype = 'gauss beam',probe = 'neutron pol spin flip',beamw = 0.2,resintrange = 3,tthoff = 0.0,
                         pol = 'uu',wavelength = 4.4,respoints = 15,Ibkg = 0.0,I0 = 2,samplelen = 50.0,
                         restype = 'full conv and varying res.',coords = 'q',res = 0.001,incangle = 0.0)
 fp.set_wavelength(inst.wavelength)
@@ -28,7 +28,7 @@ Amb = model.Layer(b = 0, d = 0.0, f = (1e-20+1e-20j), dens = 1.0, magn_ang = 0.0
 SiO = model.Layer(b = bc.Si + bc.O*2, d = 1205, f = (1e-20+1e-20j), dens = 0.026, magn_ang = 0.0, sigma = 2, xs_ai = 0.0, magn = 0.0)
 Sub = model.Layer(b = bc.Si, d = 0.0, f = (1e-20+1e-20j), dens = 8/5.443**3, magn_ang = 0.0, sigma = 2, xs_ai = 0.0, magn = 0.0)
 
-surf = model.Stack(Layers=[SiO], Repetitions = 1)
+surf = model.Stack(Layers=[SiO], Repetitions = 100)
 
 sample = model.Sample(Stacks = [surf], Ambient = Amb, Substrate = Sub)
 # END Sample
@@ -52,6 +52,15 @@ class empty():
 e=empty()
 e.x=linspace(0.001, 0.3, 500)
 
-for i in range(10000):
+print("Numba")
+model.Paratt.ReflQ=paratt_numba.ReflQ
+model.MatrixNeutron.Refl=neutron_numba.Refl
+for i in range(100):
+    model.Buffer.parameters=None
     Sim([e])
-print("")
+print("Cuda")
+model.Paratt.ReflQ=paratt_cuda.ReflQ
+model.MatrixNeutron.Refl=neutron_cuda.Refl
+for i in range(100):
+    model.Buffer.parameters=None
+    Sim([e])
