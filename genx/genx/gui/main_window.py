8c7c78109d9e97c10d872d03bf7c75df3d1f5cf3
Artur Glavic <artur.glavic@psi.ch>
2024-07-04 16:19:30 +0200

    add numba dependency

--- genx/genx/gui/main_window.py
+++ genx/genx/gui/main_window.py
@@ -990,7 +990,7 @@ class GenxMainWindow(wx.Frame, conf_mod.Configurable):
             [p.ReadConfig() for p in self.get_pages() if hasattr(p, 'ReadConfig')]
         with self.catch_error(action='open_model', step=f'loading config for parameter grid'):
             self.paramter_grid.ReadConfig()
-            self.mb_checkables[custom_ids.MenuId.TOGGLE_SLIDER].Check(self.paramter_grid.GetValueEditorSlider())
+            self.mb_checkables[custom_ids.MenuId.TOGGLE_SLIDER].Check(bool(self.paramter_grid.GetValueEditorSlider()))
         debug('open_model: update plugins')
         with self.catch_error(action='open_model', step=f'processing plugins'):
             self.plugin_control.OnOpenModel(None)
