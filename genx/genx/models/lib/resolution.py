6f74eb8af68a07b4bb8d16b9e198c96517f7b049
Artur Glavic <artur.glavic@psi.ch>
2024-07-24 20:01:46 +0200

    Start TrapozoidResolution

--- genx/genx/models/lib/resolution.py
+++ genx/genx/models/lib/resolution.py
@@ -32,3 +32,24 @@ class GaussianResolution(Resolution):
     def __call__(self, TwoThetaQz, respoints=15, resintrange=2.0):
         # (TwoThetaQz, weight) =
         return ResolutionVector(TwoThetaQz[:], self.sigma, respoints, range=resintrange)
+
+@dataclass
+class TrapezoidResolution(Resolution):
+    inner_width: Union[float, np.ndarray] = 0.2
+    outer_width: Union[float, np.ndarray] = 0.3
+
+    def __call__(self, TwoThetaQz, respoints=15, resintrange=2.0):
+        # (TwoThetaQz, weight) =
+        a = -self.outer_width/2.
+        b = -self.inner_width/2.
+        c = self.inner_width/2.
+        d = self.outer_width/2.
+        scale = np.linspace(-0.5, 0.5, respoints)[:,np.newaxis]
+        Qres = TwoThetaQz+self.outer_width*scale
+        norm = 1./(self.inner_width+self.outer_width)
+        inner_rel = self.inner_width/self.outer_width
+        #weight = norm*np.where(2*abs(scale)<=inner_rel, 1.0, (x-a)/(b-a))
+        return (Qres.flatten(), weight)
+    # Qres = Q+dQ*linspace(-range, range, points)[:, newaxis]
+    # weight = 1/sqrt(2*pi)/dQ*exp(-(transpose(Q[:, newaxis])-Qres)**2/dQ**2/2)
+    # Qret = Qres.flatten()
