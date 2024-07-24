39b3910c0f49d3fd3a0bf89f6f1d8a8a0fe84ce4
Artur Glavic <artur.glavic@psi.ch>
2024-07-24 16:45:12 +0200

    Add trapezoidal beam profile

--- genx/genx/models/lib/footprint.py
+++ genx/genx/models/lib/footprint.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass, fields
 from .instrument import GaussIntensity
 from .base import ModelParamBase
 
-__all__ = ['GaussianBeamOffset', 'SquareBeamOffset']
+__all__ = ['GaussianBeamOffset', 'SquareBeamOffset', 'TrapezoidBeam']
 
 class Footprint(ModelParamBase):
     """
@@ -53,3 +53,30 @@ class SquareBeamOffset(Footprint):
         c_min = -cross_section/2.0+self.offset
         FP = np.maximum(0., (np.minimum(c_max, self.width/2.)-np.maximum(c_min, -self.width/2.)))/self.width
         return FP
+
+@dataclass
+class TrapezoidBeam(Footprint):
+    """
+    The footpring for a trapezoid beam. The intensity of the beam is constant
+    on the inner_width and then reduces linearly until outer_width is reached.
+
+    This footprint describes the typical neutron instrument situation, where the beam
+    is defined by two slits, very well.
+    """
+    inner_width: float = 0.2
+    outer_width: float = 0.3
+
+    def __call__(self, theta, samplen):
+        sinalpha = np.sin(theta*np.pi/180.)
+        cross_section = samplen*sinalpha
+        in_out_ratio = min(1.0, 2.*self.inner_width / (self.inner_width+self.outer_width))
+        if self.inner_width>0:
+            FP_in = np.minimum(1.0, cross_section/self.inner_width)
+        else:
+            FP_in = 0.0
+        if self.outer_width>self.inner_width:
+            FP_out = np.minimum(1.0, np.maximum(0.,
+                                1.0-(1.0-np.minimum(1.0, (cross_section-self.inner_width)/(self.outer_width-self.inner_width)))**2))
+        else:
+            FP_out = 0.0
+        return FP_in*in_out_ratio + FP_out*(1.-in_out_ratio)
