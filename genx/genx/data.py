4120ad4cd47f3707d0af4a7110cd666cbf13cad5
Artur Glavic <artur.glavic@psi.ch>
2024-07-30 09:45:33 +0200

    Fix errors when using some numpy functions within column calculation.

--- genx/genx/data.py
+++ genx/genx/data.py
@@ -6,8 +6,9 @@ on set and the class DataList stores multiple DataSets.
 import time
 import os
 from sys import platform
-from numpy import (pi, sqrt, sin, cos, tan, arcsin, arccos, arctan, arctan2, real, imag, abs, cumsum, zeros, isscalar,
-                   where, ones, linspace, exp, newaxis, transpose, arange, convolve, r_, array, not_equal, logical_not,
+from numpy import (pi, sqrt, sin, cos, tan, arcsin, arccos, arctan, arctan2, real, imag, abs, cumsum, isscalar,
+                   exp, log, log2, log10, zeros, ones, zeros_like, ones_like,
+                   where, ones, linspace, newaxis, transpose, arange, convolve, r_, array, not_equal, logical_not,
                    sum, dot, minimum, maximum, float32, float64, complex128, ndarray, float32, complex64, c_, r_,
                    loadtxt, savetxt, hstack, nan)
 from typing import List, Dict, Tuple, Union
