d3c73c08707a9ee02d3a61efbdf304ea69864bcf
Artur Glavic <artur.glavic@psi.ch>
2024-08-07 15:50:14 +0200

    Add test for example project files using api

---
+++ genx/tests/test_examples.py
@@ -0,0 +1,21 @@
+"""
+Load all example files, compile the script and make a simple simulation.
+"""
+
+import os
+import unittest
+
+from glob import glob
+
+from genx import api
+
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "genx", "examples")
+
+
+class TestExamples(unittest.TestCase):
+    def test_all(self):
+        fls = glob(os.path.join(BASE_DIR, "*.hgx"))
+        for i, fl in enumerate(fls):
+            with self.subTest(f"Datafile {fl}...", i=i):
+                model, optimizer = api.load(fl)
+                model.simulate()
