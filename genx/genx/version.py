d29dc7eddb65b977d3e397ba2fe0098064d18404
Artur Glavic <artur.glavic@psi.ch>
2025-01-31 11:00:55 +0100

    Prepare automatic version increment and tagging

--- genx/genx/version.py
+++ genx/genx/version.py
@@ -1,1 +1,21 @@
-__version__ = "3.7.5"
+__version__ = "3.7.6"
+
+def increment_version(minor=True, current=None):
+    if current is None:
+        current = __version__
+    import os
+    location = os.path.abspath(__file__)
+    module_lines = open(location, 'r').readlines()
+
+    full, ver, vminor = map(int, current.split('.'))
+    if minor:
+        vminor += 1
+    else:
+        ver += 1
+        vminor = 0
+    new_version = f"{full}.{ver}.{vminor}"
+    for i, line in enumerate(module_lines):
+        if line.startswith("__version__"):
+            module_lines[i] = f'__version__ = "{new_version}"\n'
+    with open(location, 'w') as f:
+        f.writelines(module_lines)
