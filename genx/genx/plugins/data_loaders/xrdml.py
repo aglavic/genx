0f488df0ed858bdd261c788684b6e69ccde621f2
Artur Glavic <artur.glavic@psi.ch>
2026-01-27 10:24:35 +0100

    Modify XRDML reader to return 2theta not q

--- genx/genx/plugins/data_loaders/xrdml.py
+++ genx/genx/plugins/data_loaders/xrdml.py
@@ -31,13 +31,6 @@ class Plugin(Template):
 
     def __init__(self, parent):
         Template.__init__(self, parent)
-        self.x_col = 0
-        self.y_col = 1
-        self.e_col = 2
-        self.xe_col = -1
-        self.comment = "#"
-        self.skip_rows = 0
-        self.delimiter = None
 
     def CountDatasets(self, file_path):
         try:
@@ -79,113 +72,6 @@ class Plugin(Template):
             dataset.meta["data_source"]["sample"]["name"] = ds[3]
             dataset.meta["data_source"]["experiment"].update(ds[4])
             dataset.meta["data_source"]["measurement"].update(ds[5])
-
-    def SettingsDialog(self):
-        """
-        This function should - if necessary implement a dialog box
-        that allows the user set import settings for example.
-        """
-        col_values = {"y": self.y_col, "x": self.x_col, "y error": self.e_col, "x error": self.xe_col}
-        misc_values = {"Comment": str(self.comment), "Skip rows": self.skip_rows, "Delimiter": str(self.delimiter)}
-        dlg = SettingsDialog(self.parent, col_values, misc_values)
-        if dlg.ShowModal() == wx.ID_OK:
-            col_values = dlg.GetColumnValues()
-            misc_values = dlg.GetMiscValues()
-            self.y_col = col_values["y"]
-            self.x_col = col_values["x"]
-            self.e_col = col_values["y error"]
-            self.xe_col = col_values["x error"]
-            self.comment = misc_values["Comment"]
-            self.skip_rows = misc_values["Skip rows"]
-            self.delimiter = misc_values["Delimiter"]
-        dlg.Destroy()
-
-
-class SettingsDialog(wx.Dialog):
-
-    def __init__(self, parent, col_values, misc_values):
-        wx.Dialog.__init__(self, parent, -1, "Data loader settings")
-
-        box_sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        # Make the box for putting in the columns
-        col_box = wx.StaticBox(self, -1, "Columns")
-        col_box_sizer = wx.StaticBoxSizer(col_box, wx.VERTICAL)
-
-        # col_values = {'y': 1,'x': 0,'y error': 1}
-        col_grid = wx.GridBagSizer(len(col_values), 2)
-        self.col_controls = col_values.copy()
-        keys = list(col_values.keys())
-        keys.sort()
-        for i, name in enumerate(keys):
-            text = wx.StaticText(self, -1, name + ": ")
-            control = wx.SpinCtrl(self)
-            control.SetRange(-10, 100)
-            control.SetValue(col_values[name])
-            col_grid.Add(text, (i, 0), flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, border=5)
-            col_grid.Add(control, (i, 1), flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, border=5)
-            self.col_controls[name] = control
-
-        col_box_sizer.Add(col_grid, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
-        box_sizer.Add(col_box_sizer, 0, wx.ALL | wx.EXPAND, 5)
-
-        col_box = wx.StaticBox(self, -1, "Misc")
-        col_box_sizer = wx.StaticBoxSizer(col_box, wx.VERTICAL)
-
-        # Lets add another box for comments and rows to skip
-        # misc_values = {'Comment': '#', 'Skip rows': 0,'Delimiter': 'None'}
-        col_grid = wx.GridBagSizer(len(misc_values), 2)
-        self.misc_controls = misc_values.copy()
-        keys = list(misc_values.keys())
-        keys.sort()
-        for i, name in enumerate(keys):
-            text = wx.StaticText(self, -1, name + ": ")
-            if type(misc_values[name]) == type(1):
-                control = wx.SpinCtrl(self)
-                control.SetRange(0, 100)
-                control.SetValue(misc_values[name])
-            else:
-                control = wx.TextCtrl(self, value=misc_values[name], style=wx.EXPAND)
-            col_grid.Add(text, (i, 0), flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, border=5)
-            col_grid.Add(control, (i, 1), flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL, border=5)
-            self.misc_controls[name] = control
-
-        col_box_sizer.Add(col_grid, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
-        box_sizer.Add(col_box_sizer, 0, wx.ALL | wx.EXPAND, 5)
-
-        button_sizer = wx.StdDialogButtonSizer()
-        okay_button = wx.Button(self, wx.ID_OK)
-        okay_button.SetDefault()
-        button_sizer.AddButton(okay_button)
-        button_sizer.AddButton(wx.Button(self, wx.ID_CANCEL))
-        button_sizer.Realize()
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(box_sizer, 1, wx.GROW, 20)
-        line = wx.StaticLine(self, -1, size=(20, -1), style=wx.LI_HORIZONTAL)
-        sizer.Add(line, 0, wx.GROW, 30)
-
-        sizer.Add(button_sizer, 0, flag=wx.ALIGN_RIGHT, border=20)
-        self.SetSizer(sizer)
-
-        sizer.Fit(self)
-        self.Layout()
-
-    def GetColumnValues(self):
-        values = {}
-        for key in self.col_controls:
-            values[key] = self.col_controls[key].GetValue()
-        return values
-
-    def GetMiscValues(self):
-        values = {}
-        for key in self.misc_controls:
-            val = self.misc_controls[key].GetValue()
-            if type(val) == type("") or type(val) == type(""):
-                if val.lower() == "none":
-                    val = None
-            values[key] = val
-        return values
 
 
 def ReadXpert(file_name):
@@ -254,10 +140,9 @@ def ReadXpert(file_name):
             th = np.linspace(moving_positions["2Theta"][0], moving_positions["2Theta"][1], len(data)) / 2.0
         else:
             th = np.linspace(moving_positions["Omega"][0], moving_positions["Omega"][1], len(data))
-        Q = 4.0 * np.pi / lamda * np.sin(th / 180.0 * np.pi)
         meta_measurement["instrument_settings"] = {
             "incident_angle": {"min": float(th.min()), "max": float(th.max()), "unit": "deg"},
             "wavelength": {"magnitude": lamda, "unit": "angstrom"},
         }
-        datasets.append((Q, I, dI, sample_name, meta_experiment, meta_measurement))
+        datasets.append((2*th, I, dI, sample_name, meta_experiment, meta_measurement))
     return datasets
